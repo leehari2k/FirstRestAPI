b59960cd75e4605892c69043bbc7de7dfc8baf8a
theleehari <nhai30928@gmail.com>
2021-02-28 18:30:29 +0700

    jwt +  auth.controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,24 @@
 const { HTTP_STATUS_CODE } = require("../constant/index");
-const hapi = require("@hapi/joi");
+const JWT = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/index");
 const User = require("../models/user.model");
+
+const encodedToken = (userID) => {
+  return JWT.sign(
+    {
+      iss: "Le Ngoc Hai",
+      id: userID,
+      iat: new Date().getTime(),
+      exp: new Date().setDate(new Date().getDate() + 1),
+    },
+    JWT_SECRET
+  );
+};
 
 const register = async (req, res, next) => {
   try {
     const { username, email, phone, password } = req.body;
     const emailExists = await User.findOne({ email });
-    console.log(emailExists);
     if (emailExists)
       return res.status(HTTP_STATUS_CODE.FORBIDDEN).json({
         error: "Email is already exists",
@@ -19,12 +31,27 @@ const register = async (req, res, next) => {
         error: "register failed",
       });
 
-    return res.status(HTTP_STATUS_CODE.CREATE).json({ user: newUser });
+    const token = encodedToken(newUser._id);
+    res.setHeader("Authorization", token);
+    return res.status(HTTP_STATUS_CODE.CREATE).json({ success: true });
   } catch (err) {
     next(err);
   }
 };
 
+const signUp = async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const user = await User.find({ email });
+    console.log(user);
+    const token = encodedToken(user._id);
+    res.setHeader("Authorization", token);
+    return res.status(HTTP_STATUS_CODE.OK).json({ success: true });
+  } catch (err) {
+    next(err);
+  }
+};
 module.exports = {
   register,
+  signUp,
 };
